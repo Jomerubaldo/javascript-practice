f0b656dadab777dea897115b29f2bbece4fe4c0b
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-11 16:33:33 +0800

    initial commit

---
+++ practice.js
@@ -0,0 +1,26 @@
+//practice using variables and alert() function with concatation +
+
+let $name = "Alenere";
+let $name1 = "David";
+let $name2 = "she";
+let $name3 = "coffee";
+
+alert(
+  $name +
+    ", " +
+    "the friendly neighbor" +
+    ", " +
+    "waved at " +
+    $name1 +
+    " as " +
+    $name2 +
+    " walked by " +
+    $name1 +
+    "'s " +
+    "house.  " +
+    $name +
+    " smiled back and invited " +
+    $name +
+    "in for a cup of " +
+    $name3
+);
