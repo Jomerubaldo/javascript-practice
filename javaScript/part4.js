c0f331b27f639fd354aa3a9268b1e1d232fd4007
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-12 16:18:49 +0800

    initial commit

---
+++ javaScript/part4.js
@@ -0,0 +1,63 @@
+//Create a variable and inside the variable computre the words and then if how many characters inside it using  text.lenght; function
+
+// let $name = "Jomer Ubaldo";
+
+// alert($name.length);
+
+//Output: 12
+
+//Create a variable then choose the letter u want to display using index
+
+// let name1 = "Jomer";
+
+// alert(name1[name1.length-5 ]);
+
+//Outoput: m
+
+//Convert to lowcase and uppercase the letter inside the variable
+
+// let userName = "Jomerubaldo";
+
+// userName = userName.toUpperCase();
+// userName1 = userName.toLowerCase();
+
+// alert(userName);
+// alert(userName1);
+
+//Output: JOMERUBALDO
+//Ouput: jomerubaldo
+
+// trim fuction() delete the space for the words
+
+// let word = " World ";
+
+// word = word.trimEnd();
+// word = word.trimStart();
+// alert(word);
+//Output: World
+
+// replace and replaceAll
+
+// let animal = "I have a cat and the cat is so cute";
+
+// animal = animal.replace("cat", "dog");
+
+// alert(animal);
+
+//Output: I have a dog and the cat is so cute
+
+// animal = animal.replaceAll("cat", "dog");
+
+// alert(animal);
+
+//Output: I have a dog and the dog is so cute
+
+// using slice() fuction to crap the word start and the end is final word to get
+
+let j = "I have a cat and the cat is so cute";
+
+j = j.slice(31, 35); // counting the words
+
+alert(j);
+
+//Output: cute
