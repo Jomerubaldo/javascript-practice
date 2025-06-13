bf6c3889b90e8c63ef138840ba5113f93b7ae26c
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-14 00:03:03 +0800

    initial commit

---
+++ javaScript/part8.js
@@ -0,0 +1,47 @@
+// let level = 4;
+
+// switch (level) {
+//   case 1:
+//     console.log("Ang pinindot mo ay ang number 1");
+//     break;
+//   case 2:
+//     console.log("Ang pinindot mo ay number 2");
+//     break;
+//   case 3:
+//     console.log("Ang pinindot mo ay number 3");
+//     break;
+//   default:
+//     console.log("Tang ina mo wala yan sa nabanggit!");
+//     break;
+// }
+
+//Switch Case
+
+// let day = 1;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wenesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid Input");
+//     break;
+// }
---
+++ javaScript/part9.js
@@ -0,0 +1,58 @@
+//While Loop Normal Function
+
+// let x = 0; // declared the value
+
+// while (x < 10) { // check if x is lessthan 10 if true the condition is run 5 times Hello World!
+//   console.log("Hello World!");
+//   x++;
+// }
+
+//While Lopp With Arrays Function with backread value inside the array
+
+// let names = ["Jomer", "Jelly", "Mercy", "Junior"];
+
+// let i = names.length - 1; // -1 it because 4-1 is 3 so sa index 3 is si Junior sa array value
+
+// checheck niya kung undefined yong value
+// while (names[i]) {
+//   console.log(names[i]);
+//   i--; // change it into decreament
+// }
+
+// while loop using break
+
+// let names = ["Jomer", "Jelly", "Mercy", "Junior"];
+
+// let i = 0;
+
+// while (names[i]) {
+//   console.log(names[i]);
+//   i++;
+
+//   if (i == 2) break;
+// }
+
+//While loop using searacahing name
+
+// let names = ["Jomer", "Jelly", "Mercy", "Junior"];
+
+// let i = 0;
+
+// let y = "Mercy"; //searaching name
+
+// while (names[i]) {
+//   if (names[i] === y) {
+//     console.log(names[i]);
+//     break;
+//   }
+//   i++;
+// }
+
+//Do while loop
+
+//  let x = 0;
+
+// do {
+//   console.log("Jomer");
+//   x++;
+// } while (x < 5);
