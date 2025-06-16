59348f9773b789830f087adf28e6f1c24b906274
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-16 21:11:46 +0800

    initial commit

--- javaScript/part11.js
+++ javaScript/part11.js
@@ -1,17 +1,23 @@
-let person = {
+let information = {
   firstName: "Jomer",
   lastName: "Ubaldo",
-  age: 22,
-  pets: {
-    name: "Kupal",
-    type: "Human",
-    ability: {
-      nen: "Fly",
-      curse: "Jump",
-    },
-  },
+  age: 21,
+  bloodType: "O",
+  breed: "Human",
+  hobbies: ["Coding", "Watching", "Gym", "Reading"],
+  animal: {
+    pet1: {
+      petName: "Buranday",
+      
+    }
+  }
 };
+console.log(
+  `This is a full name of user: ${information["firstName"]} ${information["lastName"]}`
+);
+console.log(information["hobbies"][3]);
 
-alert(person["pets"]["name"]);
-alert(person["pets"]["type"]);
-alert(person["pets"]["ability"]["nen"]);
+console.log(information["pet"]["name1"]);
+console.log(information["pet"]["breed1"]);
+console.log(information["pet"]["name2"]);
+console.log(information["pet"]["breed2"]);
